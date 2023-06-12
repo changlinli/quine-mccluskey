444adf7145cbb27c6e62c75bdeefbcd7a058c559
Changlin Li <mail@changlinli.com>
2023-06-12 13:58:53 -0700

    Clean up some undefineds

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,7 @@ import Data.Function ((&))
 main :: IO ()
 main = do
     print (calculateTableResult customInput [False,True,False,False])
-    print (goalFunction customInput 4)
+    print (goalFunction customInput)
 
 customInput :: Input
 customInput = Input
@@ -28,53 +28,66 @@ customInput = Input
             ]
     }
 
-data BooleanExpression
-    = And BooleanExpression BooleanExpression
-    | Or BooleanExpression BooleanExpression
-    | Not BooleanExpression
+data BooleanFormula
+    = And BooleanFormula BooleanFormula
+    | Or BooleanFormula BooleanFormula
+    | Not BooleanFormula
     | LogicalVariable String
+    | ConstantTrue
+    | ConstantFalse
     deriving (Eq)
 
-instance Show BooleanExpression where
-    show (And x0 x1) = show x0 ++ " * " ++ show x1
+instance Show BooleanFormula where
+    show (And x0 x1) = show x0 ++ show x1
     show (Or x0 x1) = show x0 ++ " + " ++ show x1
-    show (Not x0) = "-(" ++ show x0 ++ ")"
-    show (LogicalVariable str) = "\"" ++ str ++ "\""
+    show (Not (LogicalVariable str)) = show (LogicalVariable str) ++ "'"
+    show (Not x0) = "(" ++ show x0 ++ ")'"
+    show (LogicalVariable str) = str
+    show ConstantTrue = "TRUE"
+    show ConstantFalse = "FALSE"
 
-instance IsString BooleanExpression where
-    fromString :: String -> BooleanExpression
+instance IsString BooleanFormula where
+    fromString :: String -> BooleanFormula
     fromString = LogicalVariable
 
-myExpression :: BooleanExpression
-myExpression = "x" * "y" + "y" * "z" + (-"w")
+myFormula :: BooleanFormula
+myFormula = "x" * "y" + "y" * "z" + (-"w")
 
 data Input = Input
     { minTerms :: [[Bool]]
     , dontCare :: [[Bool]]
     }
 
+numOfBooleanVars :: Input -> Natural
+numOfBooleanVars input = (minTerms input) & head & length & fromIntegral
+
     -- I want 0 as my only minterm
     -- I don't care about 1 and 2
     -- Is this 4 boolean values with the fourth always set to 1? Is this 8 boolean values with 3, 4, 5, 6, 7 all set to 1?
 
-instance Num BooleanExpression where
-    (*) :: BooleanExpression -> BooleanExpression -> BooleanExpression
+instance Num BooleanFormula where
+    (*) :: BooleanFormula -> BooleanFormula -> BooleanFormula
     x * y = And x y
 
-    (+) :: BooleanExpression -> BooleanExpression -> BooleanExpression
+    (+) :: BooleanFormula -> BooleanFormula -> BooleanFormula
     x + y = Or x y
 
-    negate :: BooleanExpression -> BooleanExpression
+    negate :: BooleanFormula -> BooleanFormula
     negate = Not
 
-    abs :: BooleanExpression -> BooleanExpression
+    abs :: BooleanFormula -> BooleanFormula
     abs = id
 
-    signum :: BooleanExpression -> BooleanExpression
+    signum :: BooleanFormula -> BooleanFormula
     signum = id
 
-    fromInteger :: Integer -> BooleanExpression
-    fromInteger = undefined
+    fromInteger :: Integer -> BooleanFormula
+    fromInteger x
+        | x <= 0 = ConstantFalse
+        | x == 1 = ConstantTrue
+        | x > 2 = fromInteger (x - 2)
+        -- Various values of infinity
+        | otherwise = ConstantFalse
 
 data TableResult
     = DontCare
@@ -97,32 +110,39 @@ calculateTableResult input bools =
         else if isMinTerm then BoolValue True
         else BoolValue False
 
-generateTable :: Input -> Natural -> [([ Bool ], TableResult )]
-generateTable myInput totalNumOfBooleanVars =
-    fmap (\row -> (row, calculateTableResult myInput row)) (generateAllBooleanPossibilities totalNumOfBooleanVars)
+generateTable :: Input -> [([ Bool ], TableResult )]
+generateTable myInput =
+    fmap (\row -> (row, calculateTableResult myInput row)) (generateAllBooleanPossibilities (numOfBooleanVars myInput))
 
 shouldIncludeTableResult :: TableResult -> Bool
 shouldIncludeTableResult DontCare = False
 shouldIncludeTableResult (BoolValue x) = x
 
-idxToVariableName :: Natural -> String
-idxToVariableName idx = ['a'..'z']
-    & fmap (\c -> [c])
-    & drop (fromInteger. toInteger $ idx)
-    & head
+-- A fun little application of Haskell's laziness and infinite lists
+variableNames :: [ String ]
+variableNames = ['a'..'z']
+    & cycle
+    & zip [(0 :: Integer)..]
+    & fmap (\(idx, c) -> if idx < 26 then [ c ] else c : show (idx `div` 26))
 
-singleMinTermToBooleanExpression :: [ Bool ] -> BooleanExpression
+toInt :: (Integral a) => a -> Int
+toInt x = fromIntegral (toInteger x)
+
+idxToVariableName :: Natural -> String
+idxToVariableName idx = variableNames !! toInt idx
+
+singleMinTermToBooleanExpression :: [ Bool ] -> BooleanFormula
 singleMinTermToBooleanExpression bools = foldr convertBoolToBooleanExpression (1, firstElemExpression) (tail bools) & snd
     where
         firstElem = head bools
         firstElemExpression = if firstElem then "a" else Not "a"
 
-        convertBoolToBooleanExpression True (idx, currentExpression) = (idx + 1, And (LogicalVariable $ idxToVariableName idx) currentExpression)
-        convertBoolToBooleanExpression False (idx, currentExpression) = (idx + 1, And (Not (LogicalVariable $ idxToVariableName idx)) currentExpression)
+        convertBoolToBooleanExpression True (idx, currentExpression) = (idx + 1, And currentExpression (LogicalVariable $ idxToVariableName idx))
+        convertBoolToBooleanExpression False (idx, currentExpression) = (idx + 1, And currentExpression (Not (LogicalVariable $ idxToVariableName idx)))
 
 -- This doesn't do minimization yet, just finds a correct expression
-goalFunction :: Input -> Natural -> BooleanExpression
-goalFunction myInput totalNumOfBooleanVars = generateTable myInput totalNumOfBooleanVars
+goalFunction :: Input -> BooleanFormula
+goalFunction myInput = generateTable myInput
     & filter (\(_, tableResult) -> shouldIncludeTableResult tableResult)
     & fmap (singleMinTermToBooleanExpression . fst)
     & foldr1 Or
